4fc13e7504fba1b7b263f413bb0a192e5b4f9d2f
zoom <psyhozoom@gmail.com>
2018-12-24 18:15:34 +0100

    STAMPA

--- src/main/java/com/psyhozoom/virman/Client/Classes/Stampa.java
+++ src/main/java/com/psyhozoom/virman/Client/Classes/Stampa.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 import javafx.print.PageLayout;
 import javafx.print.PageOrientation;
 import javafx.print.Paper;
-import javafx.print.Printer.MarginType;
+import javafx.print.Printer;
 import javafx.print.PrinterJob;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -17,15 +17,27 @@ import org.json.JSONObject;
 public class Stampa {
 
   private PrinterJob printerJob;
+  private Printer printer;
+  private PageLayout pageLayout;
+  private Paper paper;
   private DecimalFormat dtf = new DecimalFormat("###,###,###,##0.00");
-  // Font font = Font.loadFont(getClass().getResource("font/roboto/saxmono.ttf").toExternalForm(), 10);
+  private AnchorPane anchorPane = new AnchorPane();
+  private Font font = Font.loadFont(ClassLoader.getSystemResourceAsStream("font/roboto/saxmono.ttf"), 12);
 
 
   private void stampaj(AnchorPane anchorPane, Window wind) {
+
     printerJob = PrinterJob.createPrinterJob();
     if (printerJob != null && printerJob.showPrintDialog(wind)) {
-      PageLayout pageLayout = printerJob.getPrinter()
-          .createPageLayout(Paper.A4, PageOrientation.PORTRAIT, MarginType.HARDWARE_MINIMUM);
+      pageLayout = printerJob.getPrinter().createPageLayout(Paper.A4,  PageOrientation.PORTRAIT, 0,0,0,0);
+      printerJob.getJobSettings().setPageLayout(pageLayout);
+
+      System.out.println("papir sizce: "+ printerJob.getJobSettings().getPageLayout().getPaper().getWidth()
+          + " "
+          + printerJob.getJobSettings().getPageLayout().getPaper().getHeight());
+      System.out.println("margine: "+pageLayout.getTopMargin() + " " + pageLayout.getLeftMargin());
+      System.out.println("printable size: "+ pageLayout.getPrintableWidth() + " " + pageLayout.getPrintableHeight());
+
       anchorPane.setPrefSize(pageLayout.getPrintableWidth(), pageLayout.getPrintableHeight());
       printerJob.getJobSettings().setPageLayout(pageLayout);
       System.out.println(printerJob.getPrinter().getName());
@@ -43,9 +55,6 @@ public class Stampa {
   }
 
   public void stampaUplate(Izvestaji izvestaji, Window wind) {
-    Font font = Font
-        .loadFont(ClassLoader.getSystemResourceAsStream("font/roboto/OpenSans-Regular.ttf"), 12);
-    AnchorPane anchorPane = new AnchorPane();
     Text platilac = new Text();
     platilac.setText(String.format("%s\n%s", izvestaji.getPlatioc(), izvestaji.getMestoPlatioca()));
     platilac.setFont(font);
@@ -115,7 +124,75 @@ public class Stampa {
     stampaj(anchorPane, wind);
   }
 
-  public void stampaIsplate(JSONObject object) {
+  public void stampaIsplate(Izvestaji izvestaji, Window window) {
+    Text platilac = new Text();
+    platilac.setText(String.format("%s\n%s", izvestaji.getPlatioc(), izvestaji.getMestoPlatioca()));
+    platilac.setFont(font);
+
+    Text svrhaUplate = new Text();
+    svrhaUplate.setFont(font);
+    svrhaUplate.setWrappingWidth(200);
+    svrhaUplate.setText(String.format("%s", izvestaji.getSvrhaPlacanja()));
+
+    Text primalac = new Text();
+    primalac.setFont(font);
+    primalac.setText(String.format("%s\n%s", izvestaji.getPrimaoc(), izvestaji.getMestoPrimaoca()));
+
+    Text sifraPlacanja = new Text();
+    sifraPlacanja.setFont(font);
+    sifraPlacanja.setText(izvestaji.getSifraPlacanja());
+
+    Text iznos = new Text();
+    iznos.setFont(font);
+    iznos.setText(dtf.format(izvestaji.getIznos()));
+
+    Text racunPrimaoca = new Text();
+    racunPrimaoca.setFont(font);
+    racunPrimaoca.setText(izvestaji.getRacunPrimaoca());
+
+    Text modelPozivNaBroj = new Text();
+    modelPozivNaBroj.setFont(font);
+    modelPozivNaBroj.setText(izvestaji.getModelOdobrenje());
+
+    Text  zaduzenje = new Text();
+    zaduzenje.setFont(font);
+    zaduzenje.setText(izvestaji.pozivNaBrojZaduzenje);
+
+    anchorPane.getChildren()
+        .addAll(platilac, svrhaUplate, primalac, sifraPlacanja, iznos, racunPrimaoca,
+            modelPozivNaBroj, zaduzenje);
+
+    AnchorPane.setTopAnchor(platilac, 19.0);
+    AnchorPane.setLeftAnchor(platilac, 17.0);
+
+    AnchorPane.setTopAnchor(svrhaUplate, 100.0);
+    AnchorPane.setLeftAnchor(svrhaUplate, 17.0);
+
+    AnchorPane.setTopAnchor(primalac, 170.0);
+    AnchorPane.setLeftAnchor(primalac, 17.0);
+
+    AnchorPane.setTopAnchor(sifraPlacanja, 33.0);
+    AnchorPane.setLeftAnchor(sifraPlacanja, 305.0);
+
+    AnchorPane.setTopAnchor(iznos, 33.0);
+    AnchorPane.setLeftAnchor(iznos, 420.0);
+
+    AnchorPane.setTopAnchor(racunPrimaoca, 78.0);
+    AnchorPane.setLeftAnchor(racunPrimaoca, 365.0);
+
+    AnchorPane.setTopAnchor(modelPozivNaBroj, 122.0);
+    AnchorPane.setLeftAnchor(modelPozivNaBroj, 305.0);
+
+    AnchorPane.setTopAnchor(zaduzenje, 122.0);
+    AnchorPane.setLeftAnchor(zaduzenje, 370.0);
+
+    Scene scene = new Scene(anchorPane);
+    Stage stage = new Stage();
+    stage.setScene(scene);
+    //stage.showAndWait();
+    anchorPane.getStylesheets().removeAll();
+    stampaj(anchorPane, window);
+
   }
 
   public void stampaPrenost(JSONObject object) {
